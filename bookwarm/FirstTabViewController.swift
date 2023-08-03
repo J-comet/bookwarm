c027364d5ce23615464e6812d2fda02a05be126c
hyeseong <hyeseong7848@gmail.com>
2023-08-03 23:09:34 +0900

    FirstTabViewController BaseViewControllerProtocol, extension 적용

--- bookwarm/FirstTabViewController.swift
+++ bookwarm/FirstTabViewController.swift
@@ -7,38 +7,76 @@
 
 import UIKit
 
-class FirstTabViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
+class FirstTabViewController: UIViewController, BaseViewControllerProtocol {
     
     @IBOutlet var headerCollectionView: UICollectionView!
     @IBOutlet var mainTableView: UITableView!
     
-//    var movieInfo = MovieInfo()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configNavigationBar()
+        designVC()
+        configVC()
+    }
+    
+    func configNavigationBar() {
         title = "둘러보기"
-        
-        mainTableView.dataSource = self
-        mainTableView.delegate = self
-        headerCollectionView.dataSource = self
-        headerCollectionView.delegate = self
-        
+    }
+    
+    func designVC() {
         mainTableView.separatorStyle = .none
         mainTableView.rowHeight = 100
         mainTableView.showsVerticalScrollIndicator = false
         mainTableView.sectionHeaderTopPadding = 16
-        
+    }
+    
+    func configVC() {
         let tableNib = UINib(nibName: FirstTableViewCell.identifier, bundle: nil)
         mainTableView.register(tableNib, forCellReuseIdentifier: FirstTableViewCell.identifier)
         
         let headerNib = UINib(nibName: FirstHeaderCollectionViewCell.identifier, bundle: nil)
         headerCollectionView.register(headerNib, forCellWithReuseIdentifier: FirstHeaderCollectionViewCell.identifier)
         
+        mainTableView.dataSource = self
+        mainTableView.delegate = self
+        headerCollectionView.dataSource = self
+        headerCollectionView.delegate = self
+        
         configureHeaderView()
     }
     
-    // 테이블뷰 연결
+    private func configureHeaderView() {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 12
+        let count: CGFloat = 4
+        let width: CGFloat = UIScreen.main.bounds.width - (spacing * (count + 1)) // 디바이스 너비 계산
+        
+        layout.itemSize = CGSize(width: width / count, height: (width / count) * 1.5)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)  // 컨텐츠가 잘리지 않고 자연스럽게 표시되도록
+        
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing)  // 컨텐츠가 잘리지 않고 자연스럽게 표시되도록 여백설정
+        layout.minimumLineSpacing = 10        // 셀과셀 위 아래 최소 간격 (vertical) , 좌 우 최소 간격 (horizontal)
+        layout.minimumInteritemSpacing = 0    // 셀과셀 좌 우 최소 간격 (vertical) , 위 아래 최소 간격 (horizontal)
+        
+        headerCollectionView.showsHorizontalScrollIndicator = false
+        headerCollectionView.collectionViewLayout = layout
+    }
+    
+    private func moveDetailVC(row: Movie) {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: DetailViewController.identifier) as! DetailViewController
+        vc.movieData = row
+        let nav = UINavigationController(rootViewController: vc)
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: true)
+        
+        // branch test
+    }
+}
+
+extension FirstTabViewController: UITableViewDataSource, UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return MovieInfo.list.count
     }
@@ -71,10 +109,10 @@ class FirstTabViewController: UIViewController, UITableViewDataSource, UITableVi
         moveDetailVC(row: MovieInfo.list[indexPath.row])
         tableView.reloadRows(at: [indexPath], with: .fade)
     }
-    // end 테이블뷰
-    
-    
-    // 컬렉션뷰 연결
+}
+
+extension FirstTabViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return MovieInfo.list.count
     }
@@ -93,34 +131,5 @@ class FirstTabViewController: UIViewController, UITableViewDataSource, UITableVi
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         moveDetailVC(row: MovieInfo.list[indexPath.row])
-    }
-    
-    private func configureHeaderView() {
-        let layout = UICollectionViewFlowLayout()
-        let spacing: CGFloat = 12
-        let count: CGFloat = 4
-        let width: CGFloat = UIScreen.main.bounds.width - (spacing * (count + 1)) // 디바이스 너비 계산
-        
-        layout.itemSize = CGSize(width: width / count, height: (width / count) * 1.5)
-        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)  // 컨텐츠가 잘리지 않고 자연스럽게 표시되도록
-        
-        layout.scrollDirection = .horizontal
-        layout.sectionInset = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing)  // 컨텐츠가 잘리지 않고 자연스럽게 표시되도록 여백설정
-        layout.minimumLineSpacing = 10        // 셀과셀 위 아래 최소 간격 (vertical) , 좌 우 최소 간격 (horizontal)
-        layout.minimumInteritemSpacing = 0    // 셀과셀 좌 우 최소 간격 (vertical) , 위 아래 최소 간격 (horizontal)
-        
-        headerCollectionView.showsHorizontalScrollIndicator = false
-        headerCollectionView.collectionViewLayout = layout
-    }
-    
-    private func moveDetailVC(row: Movie) {
-        let sb = UIStoryboard(name: "Main", bundle: nil)
-        let vc = sb.instantiateViewController(withIdentifier: DetailViewController.identifier) as! DetailViewController
-        vc.movieData = row
-        let nav = UINavigationController(rootViewController: vc)
-        nav.modalPresentationStyle = .fullScreen
-        present(nav, animated: true)
-        
-        // branch test
     }
 }
