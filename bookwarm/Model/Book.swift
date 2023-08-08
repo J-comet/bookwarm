9c974426a8440ebd2c2b0fb4a0a40ded38616801
hyeseong <hyeseong7848@gmail.com>
2023-08-08 21:58:06 +0900

    [ADD] BookViewController 추가, api 호출 확인 완료

---
+++ bookwarm/Model/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  bookwarm
+//
+//  Created by 장혜성 on 2023/08/08.
+//
+
+import Foundation
+
+struct Book {
+    var authors: [String]
+    var title: String
+    var contents: String
+    var thumbnail: String
+}
